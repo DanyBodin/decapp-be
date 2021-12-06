ef7707c10adafdfb162b663b216fdec2f4b9895e
DANY <dany@mbp-de-dany.lan>
2021-12-06 11:59:23 +0100

    user routes up

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const formidablemiddleware = require("express-formidable");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const app = express();
+app.use(formidablemiddleware());
+app.use(cors());
+
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/home", (req, res) => {
+  res.json({ message: "WELCOME TO DECAPP" });
+});
+
+const userRoutes = require("./routes/user");
+app.use(userRoutes);
+
+app.get("*", (req, res) => {
+  res.json({ message: "Page not found" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server up and running!`);
+});
